3590718b914e429170510375114dfbaffc02c09d
nabeelDanishC <nabeel.danish@cowlar.com>
2022-10-20 20:24:39 +0500

    Query Service Added

---
+++ query/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const queryRoutes = require("./routes/query");
+const eventsRoutes = require("./routes/events");
+
+// ENVS
+const port = process.env.PORT || 4000;
+
+// Creating the Express App
+const app = express();
+
+// Adding middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Adding Routers
+app.use(queryRoutes);
+app.use(eventsRoutes);
+
+// Default Route
+app.get("/", (req, res, next) => {
+  res.send("Query  Web API");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on Port ${port}`);
+});
---
+++ query/store/index.js
@@ -0,0 +1,12 @@
+exports.posts = {
+  cb6244e2: {
+    id: "cb6244e2",
+    title: "First Post",
+    comments: [],
+  },
+  "38667e1a": {
+    id: "38667e1a",
+    title: "Second Post",
+    comments: [],
+  },
+};
