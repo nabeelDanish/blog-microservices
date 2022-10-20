1579eee22b0c39cc46dcca1eeeb95374de532f4c
nabeelDanishC <nabeel.danish@cowlar.com>
2022-10-20 13:01:05 +0500

    Emitting events from comments

--- comments/controllers/comments.js
+++ comments/controllers/comments.js
@@ -1,4 +1,5 @@
 const { randomBytes } = require("crypto");
+const axios = require("axios");
 
 // Data
 const commentsByPostId = {
@@ -14,7 +15,7 @@ exports.getComments = (req, res, next) => {
   res.send(commentsByPostId[req.params.id] || []);
 };
 
-exports.postComment = (req, res, next) => {
+exports.postComment = async (req, res, next) => {
   const commentId = randomBytes(4).toString("hex");
 
   const postId = req.params.id;
@@ -24,6 +25,16 @@ exports.postComment = (req, res, next) => {
 
   comments.push({ id: commentId, content });
   commentsByPostId[postId] = comments;
+
+  // Emitting Events
+  await axios.post(`${process.env.EVENT_BUS_API_URL}/events`, {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
 
   res.status(201).send(comments);
 };
--- event-bus/controllers/events.js
+++ event-bus/controllers/events.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 exports.handleEvents = (req, res, next) => {
   const event = req.body;
 
-  console.log("Event Received: ", event);
+  console.log("\n-> Event Received: ", event);
 
   axios.post(`${process.env.POSTS_URL}/events`, event).catch((e) => {
     console.log("Error sending to posts");
