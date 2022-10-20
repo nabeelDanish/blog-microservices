3fc134b21cc136a58ba1229713724b16797497fb
nabeelDanishC <nabeel.danish@cowlar.com>
2022-10-20 12:56:25 +0500

    Emitting Events from Posts

--- event-bus/controllers/events.js
+++ event-bus/controllers/events.js
@@ -3,14 +3,16 @@ const axios = require("axios");
 exports.handleEvents = (req, res, next) => {
   const event = req.body;
 
-  axios.post("http://localhost:4000/events", event).catch((e) => {
-    console.log(e);
+  console.log("Event Received: ", event);
+
+  axios.post(`${process.env.POSTS_URL}/events`, event).catch((e) => {
+    console.log("Error sending to posts");
   });
-  axios.post("http://localhost:4001/events", event).catch((e) => {
-    console.log(e);
+  axios.post(`${process.env.COMMENTS_URL}/events`, event).catch((e) => {
+    console.log("Error sending to comments");
   });
-  axios.post("http://localhost:4002/events", event).catch((e) => {
-    console.log(e);
+  axios.post(`${process.env.QUERY_URL}/events`, event).catch((e) => {
+    console.log("Error sending to query");
   });
 
   res.send({ status: "OK" });
--- posts/controllers/post.js
+++ posts/controllers/post.js
@@ -17,14 +17,17 @@ exports.getPosts = (req, res, next) => {
   res.send(posts);
 };
 
-exports.postPost = (req, res, next) => {
+exports.postPost = async (req, res, next) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
   posts[id] = { id, title };
 
   // Emitting an event
-  axios.post();
+  await axios.post(`${process.env.EVENT_BUS_API_URL}/events`, {
+    type: "PostCreated",
+    data: { id, title },
+  });
 
   res.status(201).send(posts[id]);
 };
