b68662f4479ad396fc4eb6155533f04ddf004119
nabeelDanishC <nabeel.danish@cowlar.com>
2022-10-28 14:39:38 +0500

    Removing starting database

--- comments/store/index.js
+++ comments/store/index.js
@@ -1,9 +1,1 @@
-exports.commentsByPostId = {
-  "38667e1a": [
-    {
-      id: "f776d2e8",
-      content: "Nice Post Dear",
-      status: "pending",
-    },
-  ],
-};
+exports.commentsByPostId = {};
--- query/store/index.js
+++ query/store/index.js
@@ -1,18 +1,1 @@
-exports.posts = {
-  cb6244e2: {
-    id: "cb6244e2",
-    title: "First Post",
-    comments: [],
-  },
-  "38667e1a": {
-    id: "38667e1a",
-    title: "Second Post",
-    comments: [
-      {
-        id: "f776d2e8",
-        content: "Nice Post Dear",
-        status: "pending",
-      },
-    ],
-  },
-};
+exports.posts = {};
